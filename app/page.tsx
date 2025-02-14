471f2aa4a30bc7b068f2119bccb9b074c0aec56e
KARN <95653930+KaranDubey400@users.noreply.github.com>
2025-02-14 13:30:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,17 @@ export default function Page() {
   return (
     <section>
       <a href={socialLinks.twitter} target="_blank">
-{/*         <Image
-          src="/converted_image.png"
-          alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
-          unoptimized
-          width={160}
-          height={160}
-          priority
-        /> */}
-<Image
+
+  <Image
   src="/converted_image.png"
   alt="Profile photo"
-  className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 hover:grayscale"
+  className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5"
   unoptimized
   width={160}
   height={160}
   priority
 />
+
 
 
       </a>
