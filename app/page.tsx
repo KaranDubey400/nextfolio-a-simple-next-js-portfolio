f6542695d6188fef962f5ea9802786cb3339d366
KARN <95653930+KaranDubey400@users.noreply.github.com>
2025-02-14 13:28:13 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <section>
       <a href={socialLinks.twitter} target="_blank">
-        <Image
+{/*         <Image
           src="/converted_image.png"
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
@@ -13,7 +13,18 @@ export default function Page() {
           width={160}
           height={160}
           priority
-        />
+        /> */}
+
+        <Image
+  src="/converted_image.png"
+  alt="Profile photo"
+  className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5"
+  unoptimized
+  width={160}
+  height={160}
+  priority
+/>
+
       </a>
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
         Software Engineer
